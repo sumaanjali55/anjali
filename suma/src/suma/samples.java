d75de68e14dd62331e7faa022473b57042a30d06
anjal <anjal@KANNA1020>
2025-09-17 20:05:00 +0530

    8 commit

---
+++ suma/src/suma/samples.java
@@ -0,0 +1,16 @@
+package suma;
+
+public class samples {
+samples(){
+	System.out.println("this is constuctor");
+}
+public void show(){
+	System.out.println("this is method");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      samples ob = new samples();
+      ob.show();
+
+	}
+}
