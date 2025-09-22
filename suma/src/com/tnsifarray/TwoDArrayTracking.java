86f98ce7794bd4c3ea20d3e6b496ca867e3078b9
anjal <anjal@KANNA1020>
2025-09-22 22:36:24 +0530

    commit

---
+++ suma/src/com/tnsifarray/TwoDArrayTracking.java
@@ -0,0 +1,21 @@
+package com.tnsifarray;
+
+public class TwoDArrayTracking {
+	    public static void main(String[] args) {
+	
+	        int[][] arr = {
+	            {1, 2, 3},
+	            {4, 5, 6},
+	            {7, 8, 9}
+	        };
+
+	        System.out.println("Tracking (traversing) the 2D Array:");
+	        for (int i = 0; i < arr.length; i++) {
+	            for (int j = 0; j < arr[i].length; j++) {
+	                System.out.print(arr[i][j] + " ");
+	            }
+	            System.out.println(); // Move to next row
+	        }
+	    }
+
+}
